1cc07a2fcb5e79144840e367bca3a5ab481f3fd7
Salih Candir <salih@live.at>
2023-10-12 10:31:06 +0200

    feat: implement solution for strings and iterators

--- day_2/strings_iterators.rs
+++ day_2/strings_iterators.rs
@@ -1,8 +1,17 @@
-// TODO: remove this when you're done with your implementation.
-#![allow(unused_variables, dead_code)]
-
 pub fn prefix_matches(prefix: &str, request_path: &str) -> bool {
-    unimplemented!()
+    // TODO: we need to handle the heading "/" char otherwise split_once returns an empty string
+    // witch is a performance penalty as unnecessary recursive checks are made
+    let prefix_str = prefix.split_once('/');
+    let request_str = request_path.split_once('/');
+
+    // TODO: i guess the exercise should be done using iterators but it's not clear
+    match (prefix_str, request_str) {
+        (Some(("*", p_rest)), Some((_, r_rest))) => prefix_matches(p_rest, r_rest),
+        (Some((p, p_rest)), Some((r, r_rest))) if p == r => prefix_matches(p_rest, r_rest),
+        (None, Some((r, _))) if prefix == r => true,
+        (None, _) => prefix == request_path,
+        _ => false,
+    }
 }
 
 #[test]
